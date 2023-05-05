ee9998c0b706916cd13896ed96cab370d7ca4e17
brendandaisy <bcase@uvm.edu>
2023-05-05 14:08:28 -0400

    update stochastic plotting

--- model-stochastic.R
+++ model-stochastic.R
@@ -13,7 +13,9 @@ run_gen_stochastic <- function(A) {
     Gm <- map_dbl(1:nh, ~sum((W[.x,] * Em[.x,]) / ifelse(1:nh == .x, 1, 2)))
     Gf <- Gf / sum(Gf)
     Gm <- Gm / sum(Gm)
-    Avec <- rmultinom(1, num_adults, c(M * Gf %*% t(Gm)))
+    L <- c(M * Gf %*% t(Gm))
+    L <- L / sum(L)
+    Avec <- rmultinom(1, num_adults, L)
     return(matrix(Avec, nh, nh))
 }
 
@@ -36,15 +38,25 @@ colnames(A) <- rownames(A) <- haplotypes
 diag(A) <- c(500, 50, 50)
 
 res_det <- run_model(A, 50, fun=run_generation)
-res_sto <- run_model(A, 50, fun=run_gen_stochastic)
+res_sto <- map_dfr(1:50, \(rep) {
+    r <- run_model(A, 50, fun=run_gen_stochastic)
+    mutate(model_summary(r), rep=rep)
+})
 
-res <- bind_rows(
-    mutate(model_summary(res_det), type="deterministic"),
-    mutate(model_summary(res_sto), type="stochastic")
-)
+res <- res_sto |> 
+    group_by(t, haplotype) |> 
+    summarize(prop=mean(prop), .groups="drop") |> 
+    relocate(haplotype, prop, t) |> 
+    mutate(type="stochastic") |> 
+    bind_rows(mutate(model_summary(res_det), type="deterministic"))
 
-ggplot(res, aes(t, prop, col=haplotype, linetype=type)) +
-    geom_line() +
+ggplot(res, aes(t, prop, col=haplotype)) +
+    geom_line(aes(group=interaction(haplotype, rep)), data=filter(res_sto, rep %in% sample(50, 5)), alpha=0.3) +
+    stat_smooth(
+        aes(ymin=after_stat(ymin), ymax=after_stat(ymax), group=haplotype), data=res_sto, 
+        geom="ribbon", col=NA, fill="gray50", alpha=0.3, linewidth=1.2
+    ) +
+    geom_line(aes(linetype=type), linewidth=1.2) +
     theme_bw()
 
 ggplot(model_summary(res, prop_genotype), aes(t, prop, col=genotype)) +
